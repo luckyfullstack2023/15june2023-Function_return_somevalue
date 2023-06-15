452974dd7d2d4e699cad636c4035f1568587b79e
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-06-15 12:38:24 +0530

    sir L-39 assignment done sir

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+
+// There are 2 thing related with functions
+
+// 2. Function calling/invoking call/invoke
+
+getmyname();
+getMySurname();
+
+// 1. Function defination is a one time process
+
+function getmyname(){ //This is not as per the camelCase standard
+    console.log("Lucky"); 
+
+}
+
+function getMySurname(){ // This function name is as per the camelCase standard
+
+    // function body
+    // Inside the funciton body i can write many statements
+    console.log("Khamari"); // STATEMENT = Instructions
+}
+
+
+// 2. function calling/invoking
+getMyAddress("Nayagarh Odisha India");
+
+
+ // 1. Function defination
+ function getMyAddress(addr){
+    console.log(addr);
+    
+}
+
+
+
---
+++ js/script2.js
@@ -0,0 +1,18 @@
+
+// Lets create a function which can add 2 numbers
+
+// 1. function defination
+
+function addTwoNumbers(n1,n2){
+
+    // every function return something
+    return n1+n2;
+}
+
+// 2. Function calling (Many Time)
+
+        // Associativity Principle
+        // Left = Right
+var result = addTwoNumbers(10,20);
+console.log(result);
+
